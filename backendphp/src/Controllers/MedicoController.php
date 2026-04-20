3822e4432566b80d525d1a0782ae9a7278b9772a
samyllemaria <samyllemaria@outlook.com>
2026-04-20 15:35:33 -0300

    fix: trata erro de duplicidade no CRM

--- backendphp/src/Controllers/MedicoController.php
+++ backendphp/src/Controllers/MedicoController.php
@@ -3,6 +3,7 @@ namespace App\Controllers;
 
 use App\Config\Database;
 use App\Models\Medico;
+use PDOException;
 
 class MedicoController {
     private ?\PDO $db;
@@ -24,27 +25,28 @@ class MedicoController {
     public function cadastrar() {
         header('Content-Type: application/json; charset=UTF-8');
 
-        $json_recebido = file_get_contents('php://input');
-        $dados = json_decode($json_recebido, true);
+        try{
+            $json_recebido = file_get_contents('php://input');
+            $dados = json_decode($json_recebido, true);
 
-        if (empty($dados['nome']) || empty($dados['crm']) || empty($dados['ufcrm'])) {
-            http_response_code(400);
-            echo json_encode(['erro' => 'Todos os campos (nome, crm, ufcrm) são obrigatórios.']);
+            if (empty($dados['nome']) || empty($dados['crm']) || empty($dados['ufcrm'])) {
+                http_response_code(400);
+                echo json_encode(['erro' => 'Todos os campos (nome, crm, ufcrm) são obrigatórios.']);
 
-            return;
-        }
+                return;
+            }
 
-        $modelMedico = new Medico($this->db);
-        $sucesso = $modelMedico->cadastrarMedico($dados);
+            $modelMedico = new Medico($this->db);
+            $modelMedico->cadastrarMedico($dados);
 
-        if ($sucesso) {
             http_response_code(201);
-            echo json_encode(['status' => 'SUCESSO', 'mensagem' => 'Médico cadastrado com sucesso!']);
-        } else {
-            http_response_code(500);
-            echo json_encode(['erro' => 'Ocorreu um erro ao salvar no banco de dados.']);
-         
-        }
+            echo json_encode(['status' => 'SUCESSO', 'mensagem' => 'Médico criado com sucesso!']);
+
+        } catch (\Exception $e) {
+                http_response_code(409);
+                echo json_encode(['erro' => $e->getMessage()]);
+            
+            }
 
     }
 }
--- backendphp/src/Models/Medico.php
+++ backendphp/src/Models/Medico.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Models;
 
+use Exception;
 use PDO;
+use PDOException;
 
 class Medico {
     private ?PDO $conexao;
@@ -19,17 +21,32 @@ class Medico {
     }
 
     public function cadastrarMedico(array $dados): bool {
-        $sql = "INSERT INTO medicos (nome, crm, ufcrm) VALUES (:nome, :crm, :ufcrm)";
-        $stmt = $this->conexao->prepare($sql);
 
         $nomeLimpo = htmlspecialchars(strip_tags($dados['nome']));
         $crmLimpo = htmlspecialchars(strip_tags($dados['crm']));
-        $ufcrmlimpo = htmlspecialchars(strip_tags($dados['ufcrm']));
+        $ufcrmLimpo = htmlspecialchars(strip_tags($dados['ufcrm']));
 
-        $stmt->bindValue(':nome', $nomeLimpo);
-        $stmt->bindValue(':crm', $crmLimpo);
-        $stmt->bindValue(':ufcrm', $ufcrmlimpo);
+        if(empty($nomeLimpo) || empty($crmLimpo) || empty($ufcrmLimpo)) {
+            throw new Exception('Nome, CRM e UF são obrigatórios.');
+        }
 
-        return $stmt->execute();
+        $sql = "INSERT INTO medicos (nome, crm, ufcrm) VALUES (:nome, :crm, :ufcrm)";
+        $stmt = $this->conexao->prepare($sql);
+
+
+        try {
+            $stmt->bindValue(':nome', $nomeLimpo);
+            $stmt->bindValue(':crm', $crmLimpo);
+            $stmt->bindValue(':ufcrm', $ufcrmLimpo);
+
+            return $stmt->execute();
+
+
+            } catch (PDOException $e) {
+                if ($e->errorInfo[1] === 1062) {
+                    throw new Exception('CRM já cadastrado');
+                }
+            throw $e;
+        }
     }
 }
